509c7832763990771fc408f60807ae63a29c6929
ARNTET <72928421+ARNTET@users.noreply.github.com>
2021-03-31 15:43:36 +0200

    Update JGAF-Pacbio_Illumina_mtDNA_assembly.sh

--- JGAF-Pacbio_Illumina_mtDNA_assembly.sh
+++ JGAF-Pacbio_Illumina_mtDNA_assembly.sh
@@ -1,43 +1,86 @@
 #!/bin/bash
-#SBATCH --partition=fast
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2000
-#SBATCH --job-name="IlluPac_Assembly"
-#SBATCH --output=JGAF-IlluPac_Assembly%j.out
 
 # Modules needed ###############################################################
+module load cutadapt
 module load NOVOPlasty
-module load fastq-join
+module load bwa
 module load samtools
 module load canu
 
-# Sample #######################################################################
+# /!\ To fill /!\ ##############################################################
+samples=()
+lengthS=${#samples[@]}
+adapter_FWD=
+adapter_REV=
+config_files=()
 
-#A# ASSEMBLY of contigs from Illumina reads
-mkdir a_Illumina_assembly
-perl NOVOPlasty3.2.pl -c config.txt
+# Step one #######################################################################
+a=0
+while (($a<$lengthS)); do
+  #A# Trimming
+  cutadapt \
+  -a $adapter_FWD \
+  -A $adapter_REV \
+  -o ${samples[a]}".R1.fq.gz" \
+  -p ${samples[a]}".R2.fq.gz" \
+  ${samples[a]}"_R1.fastq.gz" \
+  ${samples[a]}"_R2.fastq.gz"
+  perl NOVOPlasty3.2.pl \
+  -c ${config_files[a]}
+  samtools faidx b_assemblyN/${samples[a]}".novoplasty"/contigs.fasta
+  wc -l b_assemblyN/${samples[a]}".novoplasty"/contigs.fasta.fai
+  let "a=a+1"
+done
+echo Now contigs need to be blasted to identify mitochondrial coverage
 
-#B# Manual identification of mitochondrial contigs
-mkdir b_Illumina_mt
-bwa index mt_Illumina_contigs.fasta
+# /!\ To fill /!\ ##############################################################
+a=0
+while (($a<$lengthS)); do
+echo What is the upper limit to mitochondrial coverage in ${samples[a]}
+read ${samples[a]}"_Upper"
+echo What is the lower limit to mitochondrial coverage in ${samples[a]}
+read ${samples[a]}"_Lower"
+let "a=a+1"
+done
+echo Thank you for these informations
 
-#C# Mapping of Pacbio reads on Illumina contigs
-mkdir c_Pacbio_mapping
-bwa mem -t 16 \
-b_Illumina_mt/t_Illumina_contigs.fasta \
-pacbio.fastq \
-> c_Pacbio_mapping/mapped.pacbio.sam
-samtools view -b \
-c_Pacbio_mapping/mapped.pacbio.sam \
-> c_Pacbio_mapping/mapped.pacbio.bam
-samtools fastq -F4 \
-c_Pacbio_mapping/mapped.pacbio.bam \
-> c_Pacbio_mapping/mapped.pacbio.fastq
-
-#D# Assembly of contigs from Pacbio mapped reads
-mkdir d_assemblyC
-canu \
- -p lsat \
- -d d_assemblyC \
- genomeSize=0.3m \
- -pacbio-raw mapped.pacbio.fastq
+# Step two #######################################################################
+a=0
+while (($a<$lengthS)); do
+  #C# Filtering
+  mkdir c_filtering
+  cat b_assemblyN/${samples[a]}".novoplasty"/contigs.fasta.fai \
+  | sed -e $'s/_/\t/g' \
+  | sed 's/\./,/g' \
+  | awk '$6 < '${samples[a]}"_Upper"' && $6 > '${samples[a]}"_Lower"'' \
+  | cut -f 1-6 \
+  | sed -e $'s/\t/_/g' \
+  | sed 's/,/\./g' > c_filtering/${samples[a]}"list_contigs.txt"
+  xargs samtools faidx \
+  b_assemblyN/${samples[a]}".novoplasty"/contigs.fast \
+  < c_filtering/${samples[a]}"list_contigs.txt" \
+  > c_filtering/${samples[a]}"contigs_flt.fasta"
+  #D# Mapping of Pacbio reads on Illumina contigs
+  mkdir d_Pacbio_mapping
+  bwa index c_filtering/${samples[a]}"contigs_flt.fasta"
+  bwa mem -t 16 \
+  c_filtering/${samples[a]}"contigs_flt.fasta" \
+  ${#samples[@]}".pacbio.fastq" \
+  > d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.sam"
+  samtools view -b \
+  d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.sam" \
+  > d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.bam"
+  samtools fastq -F4 \
+  d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.bam" \
+  > d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.fastq"
+  #D# Assembly of contigs from Pacbio mapped reads
+  mkdir e_assemblyC
+  canu \
+  -p lsat \
+  -d e_assemblyC \
+  genomeSize=0.3m \
+  -pacbio-raw d_Pacbio_mapping/${#samples[@]}".mapped.pacbio.fastq"
+  let "a=a+1"
+done
